d176a2a62fcf02f1ef583e0df9f65b20488f47d9
Nick2901 <dudasbarbosa2@gmail.com>
2025-04-22 20:34:21 -0300

    inicio do app chat

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require ('express')
+const http = require ('http')
+const app = express()
+const server = http.createServer(app)
+const ejs = require ('ejs')
+const path = require ('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view', path.join(__dirname, 'public'))
+app.engine('html', ejs.renderFile)
+app.use('/', (req, res) => {} )
+
+
+
+server.listen(3000, () => {console.log('chat rodando em http://localhost:3000')})
+
+
