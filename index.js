6651260c41415c666732a901a6a25ace3eddee93
Nick2901 <dudasbarbosa2@gmail.com>
2025-04-29 20:40:39 -0300

    Continuação chat

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-const express = require ('express')
-const http = require ('http')
-const app = express()
-const server = http.createServer(app)
-const ejs = require ('ejs')
-const path = require ('path')
+const express = require("express");
+const http = require("http");
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('view', path.join(__dirname, 'public'))
-app.engine('html', ejs.renderFile)
-app.use('/', (req, res) => {} )
+const app = express();
+
+const server = http.createServer(app);  
+
+const ejs = require("ejs");
+const path = require('path');
+const socketIO = require('socket.io');
+const { Socket } = require("dgram");
+const io = socketIO(server)
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.set('view', path.join(__dirname, 'public'));
+
+app.engine('html', ejs.renderFile);
+
+app.use('/', (req,res)=>{
+    res.render('indes.html')
+})
+
+let messages = []
 
 
+io.on('connection', socket=>{
+    console.log('NOVO USUÁRIO CONECTADO: ' + socket.id)
 
-server.listen(3000, () => {console.log('chat rodando em http://localhost:3000')})
+    socket.emit('previousMessage', messages)
 
+    socket.on('sendMessage', data => {
+        messages.push(data)
 
+        socket.broadcast.emit('receivedMessage', data)
+    })
+})
+
+server.listen(3000, ()=>{console.log("chat rodando em http://localhost:3000")});
